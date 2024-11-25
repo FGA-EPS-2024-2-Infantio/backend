fe2a1e86fb28445e0ba15c151ec40eea5108ca9f
HenriPucci <pucci.rique1234@gmail.com>
2024-11-25 13:36:29 -0300

    Novos campos ao dto

--- application-microservice/src/students/dtos/CreateStudent.dto.ts
+++ application-microservice/src/students/dtos/CreateStudent.dto.ts
@@ -5,4 +5,36 @@ export class CreateStudentDto {
   categorie: CategorieType;
   class: ClassType;
   turn: TurnType;
+
+  dataNascimento?: string;
+  naturalidadeAluno?: string;
+  endereco?: string;
+  cep?: string;
+
+  mae?: {
+    nome: string;
+    telefone: string;
+    cpf?: string;
+    rg?: string;
+    naturalidade?: string;
+  };
+
+  pai?: {
+    nome: string;
+    telefone: string;
+    cpf?: string;
+    rg?: string;
+    naturalidade?: string;
+  };
+
+  responsaveis?: Array<{
+    nome: string;
+    parentesco: string;
+    telefone: string;
+  }>;
+
+  observacoes?: Array<{
+    titulo: string;
+    descricao: string;
+  }>;
 }
