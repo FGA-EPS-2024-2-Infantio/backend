5192d029215cf0753b0f5052f6033e1d234b3693
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2024-11-30 19:21:37 -0300

    feat(login) full login system

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { ClassModule } from './classes/class.module';
 import { NatsClientModule } from './nats-client/nats-client.module';
 import { SchoolModule } from './schools/school.module';
 import { StudentModule } from './students/student.module';
 import { TeacherModule } from './teachers/teacher.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [NatsClientModule, UsersModule, StudentModule, SchoolModule, TeacherModule, ClassModule],
+  imports: [NatsClientModule, StudentModule, SchoolModule, TeacherModule, ClassModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
